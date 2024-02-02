4f814a7071a1f65efaa16039e79f93d6f58d1240
kavindu harshana <kythonlk@gmail.com>
2024-02-02 09:22:18 +0400

    Android studio

---
+++ android.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install openjdk-19-jre-headless
+sudo apt update 
+
+sudo snap install node
+sudo snap update 
+
+sudo add-apt-repository ppa:maarten-fonville/android-studio
+sudo apt update
+sudo apt install android-studio
+
+
