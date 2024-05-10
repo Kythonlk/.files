c0687e0ee470e7ccb37f048fd1aed38460a73081
kythonlk <kythonlk@gamil.com>
2024-05-10 11:22:24 +0400

    Update simple

--- .bashrc
+++ .bashrc
@@ -1,22 +1,18 @@
 alias nd='npm run dev'
 alias nr='npm run'
-alias n='node'
+alias n='node --watch'
 alias d='cd ~/dev'
 alias v='nvim'
 alias g='git'
 alias ga='git add .'
 alias gc='git commit -m'
 alias gp='git push'
+alias gcl='git clone'
 alias gs='git status'
 alias gco='git checkout'
-
-export B=$HOME/.bashrc
 
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-
-export DOTNET_ROOT=/snap/dotnet-sdk/current
-export PATH="$PATH:/home/kyhtonllk/.dotnet/tools"
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/13.0/bin
--- arch.sh
+++ arch.sh
@@ -1,6 +1,10 @@
-sudo git -Sy
+#!/bin/bash
 
-sudo jdk-openjdk -Sy
+mkdir ~/dev && mkdir ~/dev/fix && mkdir ~/dev/side && mkdir ~/dev/chamith && mkdir ~/dev/dad
+
+sudo pacman -Syu
+sudo pacman git -Sy
+sudo pacman -Sy jdk17-openjdk
 
 curl -sS https://starship.rs/install.sh | sh
 echo "eval "$(starship init bash)"" >>~/.bashrc
@@ -11,7 +15,7 @@ git clone https://github.com/Kythonlk/My-Nvim.config.git ~/.config/nvim
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install 22
 
-sudo yay -Sy
+sudo pacman yay -Sy
 sudo yay install android-studio
 
 curl https://releases.warp.dev/stable/v0.2024.05.07.08.02.stable_02/warp-terminal-v0.2024.05.07.08.02.stable_02-1-x86_64.pkg.tar.zst > warp-terminal.pkg.tar.zst
