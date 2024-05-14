c60f3749ecc4e8d632a6e864d38a9cd2e99bbd84
Kythonlk <109597262+Kythonlk@users.noreply.github.com>
2024-05-14 09:41:05 +0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,17 +3,18 @@
 mkdir ~/dev && mkdir ~/dev/fix && mkdir ~/dev/side && mkdir ~/dev/chamith && mkdir ~/dev/dad
 
 sudo pacman -Syu
-sudo pacman git -Sy
+sudo pacman -Sy brave-browser
+sudo pacman -Sy firefox
 sudo pacman -Sy jdk17-openjdk
 
 curl -sS https://starship.rs/install.sh | sh
-echo "eval "$(starship init bash)"" >>~/.bashrc
+echo "eval "$(starship init bash)"" > ~/.bashrc
 
 sudo pacman -S neovim
-git clone https://github.com/Kythonlk/My-Nvim.config.git ~/.config/nvim
+git clone https://github.com/Kythonlk/nvim_conf.git ~/.config/nvim
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-nvm install 22
+nvm install 20
 
 sudo pacman yay -Sy
 sudo yay install android-studio
@@ -21,7 +22,7 @@ sudo yay install android-studio
 curl https://releases.warp.dev/stable/v0.2024.05.07.08.02.stable_02/warp-terminal-v0.2024.05.07.08.02.stable_02-1-x86_64.pkg.tar.zst > warp-terminal.pkg.tar.zst
 sudo pacman -U warp-terminal.pkg.tar.zst
 
-sudo pacman -S go
+sudo pacman -Sy go
 
 source ~/.bashrc
 
